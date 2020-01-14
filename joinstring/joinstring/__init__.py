4ec68aecfa4dc6a27fb56440a88f48bf739157dc
Reece Dunham <me@rdil.rocks>
2020-01-14 21:44:42 +0000

    hmm

---
+++ joinstring/joinstring/__init__.py
@@ -0,0 +1,6 @@
+"""Performance-friendly string combining utility."""
+
+
+def joinstring(string1: str, string2: str) -> str:
+    """Performance-friendly string combining function."""
+    return "".join([string1, string2])
---
+++ joinstring/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="joinstring",
+    version="1.0.0",
+    packages=setuptools.find_packages(),
+    author="Reece Dunham",
+    author_email="me@rdil.rocks",
+    url="https://github.com/RDIL/bluejay"
+)
