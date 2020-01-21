9102daea27dc62b0574fa498924baa2dc3ab1afd
Reece Dunham <me@rdil.rocks>
2020-01-21 03:47:18 +0000

    Updates to flask-rest-error-handling

--- flask-rest-error-handling/flask_rest_error_handling/__init__.py
+++ flask-rest-error-handling/flask_rest_error_handling/__init__.py
@@ -1,13 +1,21 @@
 """Provides users with JSON responses for errors."""
 
-
 import flask
 import json
 
 
-def setup_error_handling(app: flask.Flask):
-    """Sets up error handling."""
-    for code in [401, 403, 404, 500]:
+def setup_error_handling(
+    app: flask.Flask,
+    error_codes=[401, 403, 404, 500]
+):
+    """
+    Sets up error handling.
+
+    Parameters:
+        app: The flask.Flask object to configure.
+        error_codes: A list of integers or Exceptions to handle.
+    """
+    for code in error_codes:
         app.register_error_handler(
             code,
             lambda err: flask.Response(
@@ -16,6 +24,7 @@ def setup_error_handling(app: flask.Flask):
                     "code": int(str(err)[:3]),
                     "error": str(err)
                 }),
+                # gets the first 3 characters of the error code, e.g. 404
                 int(str(err)[:3]),
                 mimetype="application/json"
             )
--- flask-rest-error-handling/setup.py
+++ flask-rest-error-handling/setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="flask-rest-error-handling",
-    version="2.0.0",
+    version="3.0.0",
     packages=setuptools.find_packages(),
     author="Reece Dunham",
     author_email="me@rdil.rocks",
     url="https://github.com/RDIL/bluejay",
     description="Provides users with JSON responses for errors.",
-    install_requires=["Flask"]
+    install_requires=["Flask"],
+    license="MIT"
 )
