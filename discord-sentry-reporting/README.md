8946197cf1db61c4561bd96c5d6e7ffef4533ee6
Reece Dunham <me@rdil.rocks>
2020-01-23 13:53:46 +0000

    fixed some issues

--- discord-sentry-reporting/README.md
+++ discord-sentry-reporting/README.md
@@ -1,11 +1,15 @@
 # Discord.py Sentry Reporting
 
-This is a small compatibility library that provides functionality to report errors from discord.py to Sentry.
+This is a small compatibility library that provides functionality to report errors from Discord.py to Sentry.
 
 ## Usage
 
 ```python
 from discord_sentry_reporting import use_sentry
 
-use_sentry(discord_client_object)  # it is typically named client or bot
+use_sentry(
+    discord_client_object,  # it is typically named client or bot
+    dsn="your_dsn_here"
+    # put in any sentry keyword arguments (**kwargs) here
+)
 ```
--- discord-sentry-reporting/setup.py
+++ discord-sentry-reporting/setup.py
@@ -2,14 +2,21 @@ import setuptools
 
 setuptools.setup(
     name="discord-sentry-reporting",
-    version="1.0.0",
+    version="2.0.0",
     packages=setuptools.find_packages(),
     author="Reece Dunham",
     author_email="me@rdil.rocks",
     url="https://github.com/RDIL/bluejay",
     description="A small compatibility library that provides functionality to report errors from discord.py to Sentry.",  # noqa
-    install_requires=["discord>=1.0.0", "sentry_sdk"],
+    install_requires=["discord.py>=1.0.0", "sentry_sdk"],
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
-    license="MIT"
+    license="MIT",
+    keywords=[
+        "discord",
+        "sentry",
+        "error",
+        "reporting",
+        "compat"
+    ]
 )
