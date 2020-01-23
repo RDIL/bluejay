e82155a1bec393889145f06799777813d1b3a10f
Reece Dunham <me@rdil.rocks>
2020-01-23 14:28:26 +0000

    bugfix

--- discord-sentry-reporting/discord_sentry_reporting/__init__.py
+++ discord-sentry-reporting/discord_sentry_reporting/__init__.py
@@ -2,8 +2,15 @@ import sentry_sdk
 
 
 def use_sentry(client, **sentry_args):
+    """
+    Use this compatibility library as a bridge between Discord and Sentry.
+
+    Arguments:
+        client: the Discord client object (e.g. `discord.AutoShardedClient`)
+        sentry_args: keyword arguments to pass to the Sentry SDK
+    """
     sentry_sdk.init(sentry_args)
 
     @client.event
-    def on_error(event, *args, **kwargs):
+    async def on_error(event, *args, **kwargs):
         sentry_sdk.capture_exception(event)
--- discord-sentry-reporting/setup.py
+++ discord-sentry-reporting/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="discord-sentry-reporting",
-    version="2.0.0",
+    version="2.0.1",
     packages=setuptools.find_packages(),
     author="Reece Dunham",
     author_email="me@rdil.rocks",
