7a2354b61c28b7b2a47b96b55a2908944a07edfb
Reece Dunham <me@rdil.rocks>
2020-03-30 18:36:23 +0000

    discord-sentry-reporting: properly re-raise exceptions instead of proxying them
    Signed-off-by: Reece Dunham <me@rdil.rocks>

--- discord-sentry-reporting/discord_sentry_reporting/__init__.py
+++ discord-sentry-reporting/discord_sentry_reporting/__init__.py
@@ -1,5 +1,4 @@
 import sentry_sdk
-from discord import ClientException
 
 
 def use_sentry(client, **sentry_args):
@@ -7,17 +6,13 @@ def use_sentry(client, **sentry_args):
     Use this compatibility library as a bridge between Discord and Sentry.
 
     Arguments:
-        client: the Discord client object (e.g. `discord.AutoShardedClient`)
-        sentry_args: keyword arguments to pass to the Sentry SDK
+        client: The Discord client object (e.g. `discord.AutoShardedClient`).
+        sentry_args: Keyword arguments to pass to the Sentry SDK.
     """
-    sentry_sdk.init(sentry_args)
+
+    sentry_sdk.init(**sentry_args)
 
     @client.event
     async def on_error(event, *args, **kwargs):
-        """
-        Yes, this will throw a ValueError. THIS IS ON PURPOSE,
-        because otherwise, it just raises 'ExceptionName: coro'
-        (coro is the name of the coroutine the exception was
-        thrown on), and *doesn't give us an actual stacktrace*!
-        """
-        sentry_sdk.capture_exception(event)
+        """Don't ignore the error, causing Sentry to capture it."""
+        raise
--- discord-sentry-reporting/setup.py
+++ discord-sentry-reporting/setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="discord-sentry-reporting",
-    version="2.0.3",
+    version="2.0.4",
     packages=setuptools.find_packages(),
     author="Reece Dunham",
     author_email="me@rdil.rocks",
     url="https://github.com/RDIL/bluejay",
     description="A small compatibility library that provides functionality to report errors from discord.py to Sentry.",  # noqa
-    install_requires=["discord.py>=1.0.0", "sentry_sdk"],
+    install_requires=["sentry_sdk"],
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     license="MIT",
